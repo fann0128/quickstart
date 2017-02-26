a40a292642e749f234e7e02a01d4fb18c8d194d2
fan yang <yangfanballack@gmail.com>
2017-02-26 02:03:16 -0500

    try some function

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -2,8 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'user',
-  template: `<h1>Hello world</h1>`,
+  template: `<button (click)="sayHi()">Say Hi</button>
+            <div *ngIf ="displayMyName">
+              <h1>Hello world, {{name}}</h1>
+            </div>`,
 })
-export class UserComponent  { 
+export class UserComponent  {
+name:String;
+displayMyName:boolean;
+constructor()
+{
+  this.displayMyName = false;
+  this.name = "fan";
 
+}
+sayHi()
+{
+  if(this.displayMyName)
+  {
+    this.displayMyName = false;
+  }
+  else
+  {
+    this.displayMyName = true;
+  }
+}
 }
